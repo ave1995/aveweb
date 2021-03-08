f21ad595c31f02198307782aa8f7bea2ce9acdda
ales.vesely <ales.vesely@dataprojekt.cz>
2021-03-08 14:34:16 +0100

    První stylování navbaru

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,13 +3,13 @@ import 'tailwindcss/tailwind.css';
 
 export const Navbar = () => {
     return (
-        <nav className="text-lg font-semibold">
-            <div>
-                <div className="links">
-                    <Link to="/">Aleš Veselý</Link>
-                    <Link to="/about">O mně</Link>
-                    <Link to="/skills">Dovednosti</Link>
-                </div>
+        <nav className="flex border-b-2 border-indigo-700 space-x-8 font-serif md:font-Times ">
+            <div className="uppercase text-xl pl-2 pr-4">
+                <Link to="/" className="tracking-widest" >Aleš Veselý</Link>
+            </div>
+            <div className="space-x-8">
+                <Link to="/about" className="">O mně</Link>
+                <Link to="/skills" className="">Dovednosti</Link>
             </div>
         </nav>
     );
