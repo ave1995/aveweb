e46c259bdaef7ecac922f3d4c8f1b2202d8346ff
maorisun <jacobmaorisun@seznam.cz>
2021-03-08 16:50:35 +0100

    bez css

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,17 +1,45 @@
 import { Link } from 'react-router-dom'
-import 'tailwindcss/tailwind.css';
 
 export const Navbar = () => {
     return (
-        <nav className="flex border-b-2 border-indigo-700 space-x-8 font-serif md:font-Times ">
-            <div className="uppercase text-xl pl-2 pr-4">
-                <Link to="/" className="tracking-widest" >Aleš Veselý</Link>
+        <nav className="">
+            <div className="">
+                <Link to="/" className="" >Aleš Veselý</Link>
             </div>
-            <div className="space-x-8">
-                <Link to="/about" className="">O mně</Link>
-                <Link to="/skills" className="">Dovednosti</Link>
+            <div className="">
+                <ul className="">
+                    <li className="">
+                        <Link to="/about">O mně</Link>
+                    </li>
+                    <li className="">
+                        <Link to="/skills">Dovednosti</Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
 }
+
+
+// import 'tailwindcss/tailwind.css';
+
+// export const Navbar = () => {
+//     return (
+//         <nav className="flex border-b-2 border-indigo-700 space-x-8 font-serif md:font-Times ">
+//             <div className="font-black uppercase text-xl pl-2 pr-4">
+//                 <Link to="/" className="tracking-widest" >Aleš Veselý</Link>
+//             </div>
+//             <div className="space-x-8">
+//                 <ul className="inline-block">
+//                     <li className="leading-none">
+//                         <Link to="/about">O mně</Link>
+//                     </li>
+//                     <li className="leading-none">
+//                         <Link to="/skills">Dovednosti</Link>
+//                     </li>
+//                 </ul>
+//             </div>
+//         </nav>
+//     );
+// }
 
