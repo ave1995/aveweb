7783870d7c88435f2935c3d1f4c3ad094b0ccd8f
ales.vesely <ales.vesely@dataprojekt.cz>
2021-03-14 14:41:17 +0100

    nervování u nedokončeného jsonu

--- src/components/NavbarNew.tsx
+++ src/components/NavbarNew.tsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as GoIcons from 'react-icons/go'
 
+import * as SidebarData from '../data/SidebarData.json'
+
 export const NavbarNew = () => {
+
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
         <header>
             <div className="nbar">
                 <Link to="/Activity" className="menu-bars">
-                    <GoIcons.GoMarkGithub />
+                    <GoIcons.GoMarkGithub onClick={showSidebar} />
                 </Link>
             </div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className="nav-menu-items">
+                    <li className="navbar-toggle">
+                        <Link to="/Activity" className="menu-bars">
+                            <GoIcons.GoX />
+                        </Link>
+                    </li>
+
+                </ul>
+            </nav>
         </header>
     )
 }
---
+++ src/data/routes.tsx
@@ -0,0 +1,18 @@
+export const routes = [
+    {
+        index: true,
+        label: "Aleš Veselý",
+        path: '/',
+    },
+    {
+        label: 'About',
+        path: '/about',
+    },
+    {
+        label: 'Skills',
+        path: '/skills'
+    }
+
+]
+
+export default routes;
